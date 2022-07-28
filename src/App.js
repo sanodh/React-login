e1c632b09aedeea1f525ebfc79d8bf75aeb6b23f
sanodh <sanodhs@gmail.com>
2022-07-28 23:33:18 +0530

    login form updated and css file created

--- src/App.js
+++ src/App.js
@@ -14,10 +14,20 @@ function App() {
 
   const Login = details =>{
     console.log(details);
+    if (details.email == adminUser.email && details.password ==adminUser.password){
+      console.log("Logged in");
+      setUser({
+        name:details.name,
+        email:details.email
+      });
+    }else{
+      console.log("Details do not match!");
+      setError("Details do not match!");
+    }
   }
 
   const Logout = () =>{
-    console.log("Logout");
+    setUser({ name: "", email: ""});
   }
 
   return (
@@ -25,10 +35,10 @@ function App() {
       {(user.email != "") ? (
         <div className="Welcome">
         <h2>Welcome, <span>{user.name}</span></h2>
-        <button>Logout</button>
+        <button onClick={Logout}>Logout</button>
         </div>
       ) :(
-        <LoginForm/>
+        <LoginForm Login={Login} error={error}/>
       )}
     </div>
   );
